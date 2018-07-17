66d7c95e4dcae7b280c41370b645a33a8468c989
StanleyLiu <stanley_liu@accton.com>
2018-07-17 13:05:11 +0800

    Reset/Release CDEC PCIE before PCIE init

--- board/freescale/t600/pci.c
+++ board/freescale/t600/pci.c
@@ -14,6 +14,22 @@
 
 void pci_init_board(void)
 {
+	unsigned int dir = 0x00800000, val;
+	ccsr_gpio_t *gpio4 = (void *)(CONFIG_SYS_MPC85xx_GPIO_ADDR + 0x3000);
+
+	/* Since CDEC PCIE reset is triggered by reset sequence, need to reset/release manually */
+	/* Set GPIO4_8 as 0 to reset CDEC */
+	dir |= (in_be32(&gpio4->gpdir) & 0xff7fffff);
+	val = (in_be32(&gpio4->gpdat) & 0xff7fffff);
+	out_be32(&gpio4->gpdat, val);
+	out_be32(&gpio4->gpdir, dir);
+	udelay(100000);
+
+	/* Set GPIO4_8 as 1 to release CDEC */
+	val |= 0x00800000;
+	out_be32(&gpio4->gpdat, val);
+	udelay(100000);
+
 	fsl_pcie_init_board(0);
 }
 
